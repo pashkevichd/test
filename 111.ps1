69b65489347ff3853d280e46b98ab2085a405994
Dzmitriy Pashkevich <Dzmitriy_P@MAIN.VELCOM.BY>
2019-07-18 11:31:40 +0300

    last

--- 111.ps1
+++ 111.ps1
@@ -1,8 +1,29 @@
 get-service
 
 Get-Process
-
+erfreferfreerfreferfre 
 Get-AdlCatalogItem
 Get-ComputerInfo
 Get-ADuser dzmitriy_p
 Get-ADClaimType
+getmac.function Get-TargetResource {
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    param (
+    )
+}
+function Set-TargetResource {
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    param (
+    )
+}
+function Test-TargetResource {
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    param (
+    )
+}
